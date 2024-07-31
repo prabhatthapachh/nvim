e80e5ca9d58b56989ff8c64ed48fc0b5fcbde756
Prabhat Thapa <prabhat@aerion.com.au>
2024-07-31 23:32:57 +0545

    add mini.vim plugin, replace new watch key config

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,39 +1,32 @@
 return {
-    {
-        'folke/which-key.nvim',
-        config = function()
-            require('which-key').setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-            local wk = require("which-key")
-
-            wk.register({
-                f = { name = "+Find" },
-                ["fl"] = { name = "+Find LSP" },
-                ["fls"] = { name = "+Find LSP Symbol" },
-
-                t = { name = "+Todo" },
-                -- x = { name = "+Trouble" },
-
-                n = { name = "+Notes" },
-                ["nw"] = { name = "+Workspace" },
-                ["nm"] = { name = "+Mode" },
-
-                l = { name = "+LSP" },
-                ["ls"] = { name = "+LSP Symbol" },
-                -- d = { name = "+Diagnostic" },
-
-                -- w = { name = "+Workspace" },
-
-                g = { name = "+Git" },
-
-                h = { name = "+Harpoon" },
-
-                -- r = { name = "+Run" },
-
-            }, { prefix = "<leader>" })
-        end,
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
     },
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
+    config = function()
+        local wk = require("which-key")
+
+        wk.add({
+            { "<leader>f",   group = "+File" },
+            { "<leader>fl",  group = "+Fnd LSP" },
+            { "<leader>fls", group = "+Find LSP Symbol" },
+            { "<leader>l",   group = "LSP" },
+            { "<leader>ls",  group = "LSP Symbol" },
+            { "<leader>t",   group = "+Todo" },
+            { "<leader>g",   group = "+Git" },
+            { "<leader>h",   group = "+Harpoon" },
+        })
+    end
 }
